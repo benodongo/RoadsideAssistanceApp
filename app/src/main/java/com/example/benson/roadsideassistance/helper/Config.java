895bcd5cf09045a8132cdfb0d2c112f3dc5be7e1
benodogo <2013odongo@gmail.com>
2016-11-14 12:09:41 +0300

    breakdowns

---
+++ app/src/main/java/com/example/benson/roadsideassistance/helper/Config.java
@@ -0,0 +1,8 @@
+package com.example.benson.roadsideassistance.helper;
+
+/**
+ * Created by benson on 11/12/16.
+ */
+
+public class Config {
+}
---
+++ app/src/main/java/com/example/benson/roadsideassistance/models/Location.java
@@ -0,0 +1,8 @@
+package com.example.benson.roadsideassistance.models;
+
+/**
+ * Created by benson on 10/30/16.
+ */
+
+public class Location {
+}
